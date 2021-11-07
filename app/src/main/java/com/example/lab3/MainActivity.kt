3f97b38676a928ebdf0c55ad94750ac7c38431e1
IvacsonIngrid <79316730+IvacsonIngrid@users.noreply.github.com>
2021-11-07 17:28:58 +0200

    lab3 android

---
+++ app/src/main/java/com/example/lab3/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.lab3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun View.displayToast()
+    {
+        Toast.makeText(getApplicationContext(),"Wait a minute...",Toast.LENGTH_SHORT).show()
+    }
+}
